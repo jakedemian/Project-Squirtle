e7d023e4b529403af4e43c7f1917cf80a6dae96a
Jake Demian <jakedemian@gmail.com>
2022-01-15 15:40:35 -0500

    implemented basic player movement based on elipse equation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,47 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    void Start() {
+    public float horizMoveSpeed;
+    public float vertMoveSpeed;
+
+    private void Update() {
+        var hInput = Input.GetAxisRaw("Horizontal");
+        var vInput = Input.GetAxisRaw("Vertical");
+
+        var horizComponent = 0f;
+        var vertComponent = 0f;
+
+        if (hInput != 0) {
+            horizComponent = horizMoveSpeed * hInput;
+        }
+
+        if (vInput != 0) {
+            vertComponent = vertMoveSpeed * vInput;
+        }
+
+        var translation = new Vector2(horizComponent, vertComponent);
+
+        if (!Mathf.Approximately(horizComponent, 0f) && !Mathf.Approximately(vertComponent, 0f)) {
+            var a = horizMoveSpeed;
+            var b = vertMoveSpeed;
+            var x = horizComponent;
+            var y = vertComponent;
+
+            var theta = Mathf.Atan(y / x);
+            var sinTheta = Mathf.Sin(theta);
+            var cosTheta = Mathf.Cos(theta);
+
+            var sinComponent = Mathf.Pow(x, 2) * Mathf.Pow(sinTheta, 2);
+            var cosComponent = Mathf.Pow(y, 2) * Mathf.Pow(cosTheta, 2);
+
+            var r = a * b / Mathf.Sqrt(sinComponent + cosComponent);
+
+            // TODO seems scuffed, there's probly a math reason why the directions are reversed while moving left. look into this
+            var reverseFlag = horizComponent < 0f ? -1f : 1f;
+
+            translation = new Vector2(Mathf.Cos(theta) * r * reverseFlag, Mathf.Sin(theta) * r * reverseFlag);
+        }
+
+        transform.Translate(translation * Time.deltaTime);
     }
-
-    void Update() {
-
-    }
-
 }
---
+++ Assets/Scripts/Util/JLog.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Util {
+    public class JLog {
+        public static void Log(params string[] strings) {
+            var msg = "";
+            foreach (var s in strings) {
+                msg += s + " ";
+            }
+
+            Debug.Log(msg);
+        }
+
+        public static void Log(params object[] objects) {
+            var msg = "";
+            foreach (var o in objects) {
+                msg += o + " ";
+            }
+
+            Debug.Log(msg);
+        }
+    }
+}
